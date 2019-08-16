98f20b3b54cc92a1a3cf2030e1ef820f371dc71d
TELstatic <telstatic@gmail.com>
2019-08-16 17:43:54 +0800

    Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 offset-4">
+                <h1>Hello world!</h1>
+            </div>
+
+        </div>
+    </div>
+
+
     <div id="app">
+
         <example-component :user="{{\Illuminate\Support\Facades\Auth::user()}}"></example-component>
     </div>
 @endsection
